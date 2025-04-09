3a459b3831f4cb05e562f84864f4620a47c89858
Amir Nikanjam <45296036+amirniki2012@users.noreply.github.com>
2025-04-09 13:05:54 +0330

    change body mail

--- directadmin_check_mail_count_NOT_SMTP.sh
+++ directadmin_check_mail_count_NOT_SMTP.sh
@@ -31,56 +31,8 @@ if [[ "$EMAIL_COUNT" -ge 300 ]]; then
         exit 0
     fi
 
-BODY=$(cat <<EOF
-<html>
-<head>
-<title></title>
-<style type="text/css">span.preheader{display:none!important;mso-hide:all;}</style>
-</head>
-<body>
-<table align="center" border="0" cellpadding="0" cellspacing="0" style="background-color: #F5F7FA;table-layout: fixed; direction: rtl; font-family: iransans, iranyekan, azarmehr, vazir, vazirmatn, shabnam, system ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans' , 'Droid Sans' , 'Helvetica Neue' , sans-serif;" width="600">
-<thead>
-<tr>
-<td align="center"><a href="http://limoo.host/"><img alt="limoo host logo" src="https://limoo.host/email-template/limoo-host-logo.png" style="padding: 40px;" /></a></td>
-</tr>
-</thead>
-<tbody style="background-color: white;">
-<tr>
-<td style="padding: 40px;">
-    با سلام
-    <br>
-    کاربر گرامی
-    <br>
-    ارسال ایمیل از سمت سرویس شما با نام کاربری <strong>$USER</strong> در سرور <strong>$HOSTNAME</strong> از محدودیت روزانه عبور کرده است.
-    <br>
-    با توجه به قوانین تعداد ارسال ایمیل روزانه در هاست‌های اشتراکی، ارسال ایمیل شما تا ۱۲ بامداد محدود خواهد ماند.
-    <br>
-    در صورت نیاز به ارسال ایمیل بیش از ۳۰۰ عدد در روز پیشنهاد می‌کنیم نسبت به تهیه هاست ایمیل اقدام نمایید.
-</td>
-</tr>
-</tbody>
-<tfoot style="background-color: #F5F7FA;">
-<tr>
-<td style="padding: 0 40px;">
-<div style="border-bottom: 1px solid #D2D5DA; padding: 24px 0; text-align: center;">
-<p style="font-size: 12px; font-weight: 500; margin:0">حرف‌های زیادی برای گفتن داریم؛ ما را در شبکه‌های اجتماعی دنبال کنید.</p>
-<div style="margin-top:16px">
-<a href="http://limoo.host/" style="text-decoration: none; margin: 0 10px;"><img alt="limoo host website address" src="https://limoo.host/email-template/domain-icon.png" /></a>
-<a href="https://t.me/limoohost" style="text-decoration: none;margin: 0 10px;"><img alt="limoo host instagram address" src="https://limoo.host/email-template/instagram-icon.png" /></a>
-<a href="https://www.instagram.com/limoo.host" style="text-decoration: none;margin: 0 10px;"><img alt="limoo host telegram address" src="https://limoo.host/email-template/telegram-icon.png" /></a>
-<a href="https://www.twitter.com/limoohost" style="text-decoration: none; margin: 0 10px;"><img alt="twitter limoo host address" src="https://limoo.host/email-template/twitter-icon.png" /></a>
-<a href="https://www.youtube.com/@limoohost8320" style="text-decoration: none; margin: 0 10px;"><img alt="twitter limoo host address" src="https://limoo.host/email-template/youtube-icon.png" /></a>
-<a href="https://www.linkedin.com/company/limoo-host/" style="text-decoration: none; margin: 0 10px;"><img alt="twitter limoo host address" src="https://limoo.host/email-template/linkedin-icon.png" /></a>
-</div>
-</div>
-</td>
-</tr>
-</tfoot>
-</table>
-</body>
-</html>
-EOF
-)
+BODY="email Body"
+
 
 (
 echo "From: LimooHost <noreply@limoo.host>"
